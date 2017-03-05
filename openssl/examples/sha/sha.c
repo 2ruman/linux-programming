f3023474289132b6676cc578e40c76da0b1bb02c
Truman <truman.t.kim@gmail.com>
2017-03-05 09:36:07 -0800

    Modify negligible part

--- openssl/examples/sha/sha.c
+++ openssl/examples/sha/sha.c
@@ -8,11 +8,11 @@
 #include "sha.h"
 
 unsigned char *do_sha256(char *msg, unsigned char *out) {
-    return SHA256((unsigned char *)msg, strlen(msg), out);
+    return SHA256((const unsigned char *)msg, strlen(msg), out);
 }
 
 unsigned char *do_sha512(char *msg, unsigned char *out) {
-    return SHA512((unsigned char *)msg, strlen(msg), out);
+    return SHA512((const unsigned char *)msg, strlen(msg), out);
 }
 
 
