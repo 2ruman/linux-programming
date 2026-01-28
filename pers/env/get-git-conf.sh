82d2e94bf804348cbc949765a672e458f1fac61e
2ruman <truman.t.kim@gmail.com>
2026-01-28 09:07:24 +0900

    pers/dev: Update git-config

--- pers/env/get-git-conf.sh
+++ pers/env/get-git-conf.sh
@@ -3,5 +3,8 @@
 cat << EOF
 git config --global user.email "truman.t.kim@gmail.com"
 git config --global user.name "2ruman"
+git config --global merge.tool p4merge
+git config --global diff.tool p4merge
+git config --global difftool.prompt false
 git config --global  -l
 EOF
