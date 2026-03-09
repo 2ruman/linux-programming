8849d79f28292c3ac10309754402a16ccbd8e5b9
2ruman <truman.t.kim@gmail.com>
2026-03-10 00:03:32 +0900

    pers/dev: Upload inst-bcompare.sh

---
+++ pers/env/inst-bcompare.sh
@@ -0,0 +1,6 @@
+cd $HOME/Downloads
+mkdir bcompare
+cd bcompare
+wget https://www.scootersoftware.com/files/bcompare-5.1.7.31736_amd64.deb
+sudo apt update
+sudo apt install ./bcompare-5.1.7.31736_amd64.deb
